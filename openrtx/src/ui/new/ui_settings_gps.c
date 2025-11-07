09736c84113e826849c6142ddb368b5ceceae23c
Anthony Guerrero <anthony@anthony.moe>
2025-11-06 17:00:00 -0800

    add gps settings page

---
+++ openrtx/src/ui/new/ui_settings_gps.c
@@ -0,0 +1,74 @@
+#include <stdint.h>
+#include <string.h>
+
+#include "ui/ui_menu.h"
+#include "ui/ui_menu_dsl.h"
+#include "core/state.h"
+
+#ifdef CONFIG_GPS
+static MenuValueBinding gps_en_binding = {
+    .kind      = MENU_VAL_BOOL,
+    .ptr       = &state.settings.gps_enabled,
+    .on_change = NULL,
+};
+
+static const MenuItem m_gps_en = {
+    .kind        = MENU_NODE_VALUE,
+    .label       = "GPS Enabled",
+    .child_count = 0,
+    .children    = NULL,
+    .cb          = NULL,
+    .user        = &gps_en_binding,
+};
+#endif
+
+#ifdef CONFIG_RTC
+static MenuValueBinding gps_set_time_binding = {
+    .kind      = MENU_VAL_BOOL,
+    .ptr       = &state.settings.gpsSetTime,
+    .on_change = NULL,
+};
+
+static const MenuItem m_gps_set_time = {
+    .kind        = MENU_NODE_VALUE,
+    .label       = "GPS Set Time",
+    .child_count = 0,
+    .children    = NULL,
+    .cb          = NULL,
+    .user        = &gps_set_time_binding,
+};
+
+static MenuValueBinding utc_timezone_binding = {
+    .kind      = MENU_VAL_I32, //TODO: Not implemented yet
+    .ptr       = &state.settings.gpsSetTime,
+    .on_change = NULL,
+};
+
+static const MenuItem m_utc_timezone = {
+    .kind        = MENU_NODE_VALUE,
+    .label       = "UTC Timezone",
+    .child_count = 0,
+    .children    = NULL,
+    .cb          = NULL,
+    .user        = &utc_timezone_binding,
+};
+#endif
+
+static const MenuItem *const gps_children[] = {
+#ifdef CONFIG_GPS
+    &m_gps_en,
+#endif
+#ifdef CONFIG_RTC
+    &m_gps_set_time,
+    &m_utc_timezone,
+#endif
+};
+
+const MenuItem g_gps_settings_menu = {
+    .kind           = MENU_NODE_FOLDER,
+    .label          = "GPS",
+    .child_count    = ARRAY_LEN(gps_children),
+    .children       = gps_children,
+    .cb             = NULL,
+    .user           = NULL,
+};
